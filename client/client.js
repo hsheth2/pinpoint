57d47e53387b09df794bbcc19591211e57e8af47
omegablitz <aashishwelling@gmail.com>
2016-02-28 14:49:40 +0000

    changes

--- client/client.js
+++ client/client.js
@@ -1,12 +1,17 @@
-Tracker.autorun(function() {
+var lastPos = {lng: 0, lat: 0};
+
+setInterval(function() {
     if(Meteor.user()) {
         var pos = Geolocation.latLng();
+        
         if(pos) {
-            console.log(pos.lat);
-            console.log(pos.lng);
+            if(pos.lng!==lastPos.lng || pos.lat!==lastPos.lat) {
+                console.log("updating pos: "+pos.lat+","+pos.lng);
+                Meteor.call('updatePos', pos);
+            }
         }
         else {
             console.log("ERR-POSITION IS NULL");
         }
     }
-});
+}, 3000);
--- server/methods.js
+++ server/methods.js
@@ -1,3 +1,7 @@
 Meteor.methods({
-    
+    updatePos: function(pos) {
+        pos.time = Date.now();
+        Meteor.users.update(Meteor.userId(), {$set: {'profile.lastPos': pos}});
+        console.log("user pos updated.");
+    }
 });
