f779965b470f55ca25069b5a76c885d8d957c8b2
Harshal Sheth <hsheth2@gmail.com>
2016-04-16 23:32:06 -0400

    run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+export ROOT_URL=https://pinpoint-omegablitz.c9users.io
+export DDP_DEFAULT_CONNECTION_URL=https://pinpoint-omegablitz.c9users.io
+export PORT=443
+meteor run ios-device --mobile-server https://pinpoint-omegablitz.c9users.io
